0f39daee6005bbfdf145a5f7a1db83aec8ba3e75
Manuel Gil <mgilllo0809@g.educaand.es>
2026-06-03 12:03:08 +0200

    Sincronización de Emails Realizada

--- front/src/app/pages/enquire/enquire.ts
+++ front/src/app/pages/enquire/enquire.ts
@@ -1,30 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
-import { TranslationService } from '../../services/translation.service'; // Asegúrate de que la ruta sea correcta
-
+import { TranslationService } from '../../services/translation.service';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-enquire',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './enquire.html',
   styleUrl: './enquire.css',
 })
-
-
-// Exportación del componente Enquire
 export class Enquire implements OnInit {
 
   public lang = inject(TranslationService);
-
-  // Inyección del router
   private router = inject(Router);
 
-  // Reutilizamos el signal de register
+  // Signal para controlar la animación de éxito
   mostrarCheck = signal(false);
 
-  // Al iniciar se scrollea hasta arriba, a (0,0)
   ngOnInit() {
     scrollTo(0, 0);
     document.body.classList.remove('modal-open');
@@ -32,16 +27,32 @@ export class Enquire implements OnInit {
     document.body.style.height = 'auto';
   }
 
-  // Al enviar el cuestionario
   onSubmit(event: Event) {
     event.preventDefault();
 
-    // Activamos el overlay del check
-    this.mostrarCheck.set(true);
+    const serviceID = 'service_y1z0q5n';
+    const templateID = 'template_czzhikq';
+    const publicKey = '6qBz4RtY3H7Kyo-MI';
 
-    // Esperamos los 2.5s de la animación
-    setTimeout(() => {
-      this.router.navigate(['/mainpage']);
-    }, 2500);
+    const formElement = event.target as HTMLFormElement;
+
+    emailjs.sendForm(serviceID, templateID, formElement, publicKey)
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log('Email enviado con éxito:', result.text);
+
+          // Activamos el overlay del check a través del signal
+          this.mostrarCheck.set(true);
+
+          // Esperamos los 2.5s de la animación y redirigimos
+          setTimeout(() => {
+            this.router.navigate(['/mainpage']);
+          }, 2500);
+        },
+        (error) => {
+          console.error('Error crítico al enviar mediante EmailJS:', error);
+          alert('Error al procesar la solicitud. Inténtelo de nuevo.');
+        }
+      );
   }
 }
